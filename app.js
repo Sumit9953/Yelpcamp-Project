9f173c070a003ade9c0cf5a60c89026668fb5cd4
Santosh kumar <sumitsantosh555@gmail.com>
2022-09-20 15:40:05 +0530

    Restructuring review section

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const Campground = require("./models/campground");
 const catchAsync = require("./utils/catchAsync");
 const Review = require("./models/review");
 const campgrounds = require("./routes/campgrounds")
-
+const reviews = require("./routes/reviews")
 
 const app = express();
 app.engine('ejs',ejsMate);
@@ -28,47 +28,17 @@ db.once("open" , () => {
     console.log("Database Connected");
 });
 
-const validateCampground = (req,res,next) => {
-    const {error} = campgroundSchema.validate(req.body);
-    if(error){
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg,400)
-    }else{
-       next();
-    } 
-}
 
- const validateReview = (req,res,next) => {
-    const {error} = reviewSchema.validate(req.body);
-    if(error){
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg,400)
-    }else{
-       next();
-    }
- }
 
-app.use("/campgrounds",campgrounds)
+
+app.use("/campgrounds",campgrounds);
+app.use("/campgrounds/:id/reviews",reviews);
 
  app.get("/",function(req,res){
     res.render("home");
 });
 
-app.post("/campgrounds/:id/reviews", validateReview , catchasync(async(req,res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
 
-app.delete("/campgrounds/:id/reviews/:reviewId", catchAsync(async (req,res) => {
-    const {id,reviewId} = req.params;
-    await Campground.findByIdAndUpdate(id , {$pull:{reviews:reviewId}}); 
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-}))
 
 app.all("*",(req,res,next) => {
  next(new ExpressError('Page Not Found',404));
