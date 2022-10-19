a0767f807deb46b0f9ee93f90820df3d745d4e98
Santosh kumar <sumitsantosh555@gmail.com>
2022-10-19 11:18:06 +0530

    complete authentication

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,27 @@
 const express = require('express')
 const router = express.Router();
+const session = require("express-session");
 const catchAsync = require('../utils/catchAsync')
 const User = require('../models/user');
 const passport = require('passport');
+const LocalStrategy = require("passport-local")
 
 router.get('/register' , (req,res) => {
     res.render('users/register')
 })
 
-router.post('/register', catchAsync(async (req,res) => {
+router.post('/register', catchAsync(async (req,res,next) => {
     try{
    const {email, username, password} = req.body;
    const user = new User({email , username });
    const registeredUser = await User.register(user,password);
-   req.flash('success' ,'Welcome to Yelp camp!');
-   res.redirect('/campgrounds')
+   req.login(registeredUser, err => {
+    if(err){
+        return next(err);
+    }
+    req.flash('success' ,'Welcome to Yelp camp!');
+    res.redirect('/campgrounds')
+   })
     } catch(e){
         req.flash('error',e.message);
         res.redirect('register');
@@ -26,9 +33,10 @@ router.get('/login',(req,res) => {
     res.render('users/login')
 })
 
-router.post('/login',passport.authenticate('local' , { failureFlash:true , failureRedirect:'/login'}) , (req,res) => {
+router.post('/login',passport.authenticate('local' , { failureFlash:true , failureRedirect:'/login',keepSessionInfo: true}) , (req,res) => {
     req.flash('success','Welcome back!')
-    res.redirect('/campgrounds');
+    res.redirect(req.session.returnTo || '/campgrounds');
+    delete req.session.returnTo;
 })
 
 router.get('/logout',(req,res,next) => {
