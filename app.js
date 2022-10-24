026bf04123c88846dc82a344de4d42f53949a05b
Santosh kumar <sumitsantosh555@gmail.com>
2022-10-24 23:31:56 +0530

    upload image

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+if(process.env.NODE_ENV !== "production"){
+    require('dotenv').config();
+}
+
+console.log(process.env.SECRET);
+
 const { urlencoded } = require("express");
 const express = require("express");
 const mongoose = require("mongoose")
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,14 +3,20 @@ const router = express.Router();
 const campgrounds = require('../controllers/campgrounds')
 const catchasync = require("../utils/catchAsync");
 const catchAsync = require("../utils/catchAsync");
+const multer = require('multer')
+const upload = multer({dest: 'uploads/'});
 const {isLoggedIn,isAuthor,validateCampground} = require('../middleware')
 const path = require("path");
 const Campground = require("../models/campground");
 
 router.route('/')
       .get(catchAsync(campgrounds.index))
-      .post(isLoggedIn,validateCampground,catchAsync(campgrounds.createCampground));
-      
+    //   .post(isLoggedIn,validateCampground,catchAsync(campgrounds.createCampground));
+      .post(upload.array('image'),(req,res) => {
+        console.log(req.body,req.files);
+        res.send("It worked!");
+      });
+
 router.get("/new", isLoggedIn, campgrounds.renderNewform);
 
 router.route('/:id')
