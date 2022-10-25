a1b4f5996277402f9ea0197dc331e20119dc3784
Santosh kumar <sumitsantosh555@gmail.com>
2022-10-25 13:12:31 +0530

    upload image

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -11,10 +11,11 @@ module.exports.renderNewform = (req,res) => {
 
 module.exports.createCampground = async function(req,res,next){
     //    if(!req.body.campground) throw new ExpressError("Invalid Campground data  ",400)
-    
     const campground=  new Campground(req.body.campground);
+    campground.images =  req.files.map(f => ({url: f.path, filename: f.filename}))
     campground.author = req.user._id;
     await campground.save();
+    console.log(campground);
     req.flash('success', 'successfully made a new campground!')
     res.redirect(`/campgrounds/${campground._id}`);   
 }
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,19 +3,17 @@ const router = express.Router();
 const campgrounds = require('../controllers/campgrounds')
 const catchasync = require("../utils/catchAsync");
 const catchAsync = require("../utils/catchAsync");
+const {storage} = require("../cloudinary")
 const multer = require('multer')
-const upload = multer({dest: 'uploads/'});
+const upload = multer({storage});
 const {isLoggedIn,isAuthor,validateCampground} = require('../middleware')
 const path = require("path");
 const Campground = require("../models/campground");
 
 router.route('/')
       .get(catchAsync(campgrounds.index))
-    //   .post(isLoggedIn,validateCampground,catchAsync(campgrounds.createCampground));
-      .post(upload.array('image'),(req,res) => {
-        console.log(req.body,req.files);
-        res.send("It worked!");
-      });
+      .post(isLoggedIn, upload.array('image'),validateCampground ,catchAsync(campgrounds.createCampground));
+     
 
 router.get("/new", isLoggedIn, campgrounds.renderNewform);
 
